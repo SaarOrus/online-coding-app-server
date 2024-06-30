bd17ac0afcbffc43cdfa26254f007f1b1f0777e7
Saar Orus <saar4848@gmail.com>
2024-07-01 00:11:59 +0300

    Fix

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 const io = new Server(server, {
   cors: {
-    origin: 'https://online-coding-app-client-pw9l.onrender.com',
+    origin: 'https://online-coding-app-server-ktas.onrender.com',
     methods: ['GET', 'POST'],
   },
 });
