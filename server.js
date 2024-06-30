97bfd740eead1826d495d98f6ac6abeeff4b5271
Saar Orus <saar4848@gmail.com>
2024-07-01 01:19:38 +0300

    Update CORS settings

--- server.js
+++ server.js
@@ -11,13 +11,14 @@ const PORT = process.env.PORT || 3000;
 
 const io = new Server(server, {
   cors: {
-    origin: ['https://online-coding-app-client-pw9l.onrender.com'],
+    origin: ['http://localhost:3001', 'https://online-coding-app-client-pw9l.onrender.com'],
     methods: ['GET', 'POST'],
   },
 });
 
 app.use(cors({
-  origin: ['https://online-coding-app-client-pw9l.onrender.com'],
+  origin: ['http://localhost:3001', 'https://online-coding-app-client-pw9l.onrender.com'],
+  methods: ['GET', 'POST'],
 }));
 
 const dbPath = path.resolve(__dirname, './codeblocks.db');
